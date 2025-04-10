a7b46ec987ff71b4d6ab64b4f565bf31990207ab
Tim Zechmeister <tim.zechmeister03@gmail.com>
2025-04-11 00:18:13 +0200

    cleanup

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -1,12 +1,25 @@
 {lib, ...}: {
   imports = [
-    ./cli/home.nix
-    ./cli
-    ./de
+    ./home.nix
+
+    ./zsh
+    ./swaync
+    ./hyprland
+    ./waybar
+    ./rofi
   ];
 
   mongo.enable =
     lib.mkDefault true;
-  de.enable =
+
+  zsh.enable =
+    lib.mkDefault true;
+  hyprland.enable =
+    lib.mkDefault true;
+  swaync.enable =
+    lib.mkDefault true;
+  waybar.enable =
+    lib.mkDefault true;
+  rofi.enable =
     lib.mkDefault true;
 }
