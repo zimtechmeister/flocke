c14188d39dc611f888ff77beb828a1b0819cf1b6
Tim Zechmeister <tim.zechmeister03@gmail.com>
2025-04-15 23:41:41 +0200

    experimental toggle-keyboard changes

--- homeManagerModules/toggle-keyboard.sh
+++ homeManagerModules/toggle-keyboard.sh
@@ -3,21 +3,19 @@
 export STATUS_FILE="$XDG_RUNTIME_DIR/keyboard.status"
 
 enable_keyboard() {
-    printf "true" >"$STATUS_FILE"
+    # printf "true" >"$STATUS_FILE"
+    touch $STATUS_FILE
     hyprctl keyword '$LAPTOP_KB_ENABLED' "true" -r
 }
 
 disable_keyboard() {
-    printf "false" >"$STATUS_FILE"
+    # printf "false" >"$STATUS_FILE"
+    rm $STATUS_FILE
     hyprctl keyword '$LAPTOP_KB_ENABLED' "false" -r
 }
 
 if ! [ -f "$STATUS_FILE" ]; then
-  enable_keyboard
-else
-  if [ $(cat "$STATUS_FILE") = "true" ]; then
-    disable_keyboard
-  elif [ $(cat "$STATUS_FILE") = "false" ]; then
     enable_keyboard
-  fi
+else
+    disable_keyboard
 fi
