1370bd0e4cfde7bb8499ec6f9e67ef3f35af10c1
Tim Zechmeister <tim.zechmeister03@gmail.com>
2025-07-23 16:20:21 +0200

    improve starship prompt

--- homeManagerModules/general/starship/default.nix
+++ homeManagerModules/general/starship/default.nix
@@ -11,95 +11,57 @@
       enable = true;
       enableZshIntegration = true;
       settings = {
+        follow_symlinks = false;
+        scan_timeout = 10;
+
         format = ''
-          $fill
-          $sudo$character
+          $fill$git_status$fill
+          $character
         '';
         right_format = ''
           $directory $username[@](green)$hostname
         '';
 
-        scan_timeout = 10;
-
         fill = {
           symbol = " ";
           style = "underline green";
         };
+
         character = {
           success_symbol = "[➜](white)";
           error_symbol = "[➜](white)";
           vimcmd_symbol = "[](green)";
-          vimcmd_replace_one_symbol = "[R](yellow)";
-          vimcmd_replace_symbol = "[R](yellow)";
-          vimcmd_visual_symbol = "[V](yellow)";
+          vimcmd_replace_one_symbol = "[](red)";
+          vimcmd_replace_symbol = "[](red)";
+          vimcmd_visual_symbol = "[](purple)";
         };
+
         username = {
           style_user = "white";
           style_root = "red";
           format = "[$user]($style)";
-          disabled = false;
           show_always = true;
         };
 
         hostname = {
-          ssh_only = false;
           format = "[$hostname]($style)";
+          ssh_only = false;
           style = "white";
           trim_at = ".";
-          disabled = false;
         };
 
         directory = {
           format = "[$read_only]($read_only_style) [$path]($style)";
-          truncation_length = 8;
+          truncation_length = 4;
+          truncation_symbol = "";
           truncate_to_repo = true;
           read_only = "";
           read_only_style = "red";
           style = "green";
         };
 
-        cmd_duration = {
-          min_time = 0;
-          show_milliseconds = true;
-          disabled = false;
-          format = "[$duration]($style)";
-          style = "white";
-          show_notifications = false;
-          min_time_to_notify = 10000;
-        };
-
-        time = {
-          disabled = false;
-          format = "[$time]($style)";
-          style = "white";
-          time_format = "%T";
-          use_12hr = false;
-        };
-
-        git_branch = {
-          disabled = false;
-          format = "[$symbol$branch($remote_branch)](underline green)";
-        };
-
         git_status = {
-          disabled = false;
-          ignore_submodules = false;
-          # format = "([ \[$all_status$ahead_behind\]](underline green))";
-          format = "([ $all_status$ahead_behind](underline green))";
-        };
-
-        git_metrics = {
-          disabled = false;
-          only_nonzero_diffs = true;
-          ignore_submodules = false;
-          format = "([ +$added](underline green))([ -$deleted](underline green))";
-        };
-
-        sudo = {
-          disabled = false;
-          format = "[$symbol]($style)";
-          symbol = "|";
-          style = "green";
+          format = "([$all_status$ahead_behind](green))";
         };
       };
     };
