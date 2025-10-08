dd2d0365cc14715285d3594c8fa88ad76533d638
Tim Zechmeister <tim.zechmeister03@gmail.com>
2025-10-08 13:12:14 +0200

    fix some walker warnings

--- homeManagerModules/desktop/walker/default.nix
+++ homeManagerModules/desktop/walker/default.nix
@@ -33,12 +33,12 @@
         disable_mouse = false;
 
         keybinds = {
-          close = "Escape";
-          next = "Down";
-          previous = "Up";
-          toggle_exact = "ctrl e";
-          resume_last_query = "ctrl r";
-          quick_activate = ["F1" "F2" "F3" "F4"];
+          close = ["Escape"];
+          next = ["Down"];
+          previous = ["Up"];
+          toggle_exact = ["ctrl e"];
+          resume_last_query = ["ctrl r"];
+          quick_activate = ["F1" "F2" "F3" "F4" "F5" "F6" "F7" "F8" "F9" "F10" "F11" "F12"];
         };
         providers = {
           default = [
