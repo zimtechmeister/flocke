78cc9a27eab6fb45f313f6d033c62f7275c44fad
Tim Zechmeister <tim.zechmeister03@gmail.com>
2025-10-17 17:08:19 +0200

    harden ssh

--- nixosModules/general/openssh.nix
+++ nixosModules/general/openssh.nix
@@ -8,11 +8,22 @@
       lib.mkEnableOption "openssh";
   };
   config = lib.mkIf config.openssh.enable {
+    services.fail2ban = {
+      enable = true;
+    };
     services.openssh = {
       enable = true;
       settings = {
-        PermitRootLogin = "yes";
+        PermitRootLogin = "prohibit-password";
+        PasswordAuthentication = false;
       };
+    };
+    users.users.tim.openssh.authorizedKeys = {
+      keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJLOQ37NzU1kJ7ZIgl1t03/z7uiQ0kQ969hQ7U5nY9bY desktop"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM9wmhtXaBDDL88/DW/yzjX2Hf37kRwWgpTFjxlg+cgL tim@t480"
+      ];
+      keyFiles = [];
     };
     # NOTE: when using nixos-anywhere
     # users.users.root.openssh.authorizedKeys.keys = [
