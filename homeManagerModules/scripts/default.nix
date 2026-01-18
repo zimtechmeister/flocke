de717f2ffb4fed8f8128eb1bd5b811bee277c980
Tim Zechmeister <tim.zechmeister03@gmail.com>
2026-01-18 15:57:16 +0100

    adjust screenshot script

--- homeManagerModules/scripts/default.nix
+++ homeManagerModules/scripts/default.nix
@@ -6,7 +6,7 @@
 }: let
   toggle-keyboard = pkgs.lib.fileContents ./toggle-keyboard.sh;
   screenshot = pkgs.writeShellScriptBin "screenshot" ''
-    grim -g "$(slurp -o -c '#${config.stylix.base16Scheme.base08}ff')" -t ppm - | satty --filename - --fullscreen --output-filename $HOME/Pictures/screenshot-$(date '+%Y%m%d-%H:%M:%S').png
+    grim -g "$(slurp -o -d)" -t ppm - | satty --filename - --fullscreen --output-filename $HOME/Pictures/screenshot-$(date '+%Y%m%d-%H:%M:%S').png
   '';
 in {
   options.my.scripts.enable = lib.mkEnableOption "enables home-manager for nixBTW";
