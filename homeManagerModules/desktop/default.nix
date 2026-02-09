32dc6b11b68be0c3b8ee1fef442498d5f22b42cc
Tim Zechmeister <tim.zechmeister03@gmail.com>
2026-02-10 00:11:12 +0100

    remove swaync,waybar,hyprlock,hyprpaper,hyprsunset

--- homeManagerModules/desktop/default.nix
+++ homeManagerModules/desktop/default.nix
@@ -6,15 +6,10 @@
   imports = [
     ./hyprland
     # ./niri
-    ./swaync
-    ./waybar
     ./firefox.nix
     ./ghostty.nix
     ./hypridle.nix
-    ./hyprlock.nix
-    ./hyprpaper.nix
     ./hyprpolkitagent.nix
-    ./hyprsunset.nix
     ./mimeApps.nix
     ./noctalia.nix
     ./vesktop.nix
@@ -28,15 +23,10 @@
       hyprland.enable = lib.mkDefault true;
       hyprland.plugins.hyprscrolling.enable = lib.mkDefault false;
       # niri.enable = lib.mkDefault false;
-      swaync.enable = lib.mkDefault false;
-      waybar.enable = lib.mkDefault false;
       firefox.enable = lib.mkDefault false;
       ghostty.enable = lib.mkDefault true;
       hypridle.enable = lib.mkDefault true;
-      hyprlock.enable = lib.mkDefault false;
-      hyprpaper.enable = lib.mkDefault false;
       hyprpolkitagent.enable = lib.mkDefault true;
-      hyprsunset.enable = lib.mkDefault false;
       mimeApps.enable = lib.mkDefault true;
       noctalia.enable = lib.mkDefault true;
       vesktop.enable = lib.mkDefault true;
