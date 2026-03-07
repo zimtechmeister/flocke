bdad21953810ddb2606efb878f59039a1c169201
Tim Zechmeister <tim.zechmeister03@gmail.com>
2026-03-08 00:58:06 +0100

    update nix flake.lock

--- homeManagerModules/desktop/hyprland/default.nix
+++ homeManagerModules/desktop/hyprland/default.nix
@@ -94,9 +94,9 @@ in {
         };
         scrolling = {
           fullscreen_on_one_column = true;
-          column_width = 0.667;
+          column_width = 1.0;
           focus_fit_method = 1;
-          follow_min_visible = 0.33;
+          follow_min_visible = 0.3;
           explicit_column_widths = "0.333, 0.5, 0.667, 1.0";
           direction = "right";
         };
