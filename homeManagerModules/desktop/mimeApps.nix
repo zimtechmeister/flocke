d8e292f892b0291eba887b64c219fac5c22907e3
Tim Zechmeister <tim.zechmeister03@gmail.com>
2026-03-22 13:07:28 +0100

    better mimetypes

--- homeManagerModules/desktop/mimeApps.nix
+++ homeManagerModules/desktop/mimeApps.nix
@@ -8,25 +8,33 @@
     # location of .desktop files:
     # /run/current-system/sw/share/applications
     # ~/.local/share/applications
-    xdg.mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "text/*" = ["nvim.desktop"];
+    xdg = {
+      terminal-exec = {
+        enable = true;
+      };
+      mimeApps = {
+        enable = true;
+        settings = {
+          default = ["ghostty.desktop"];
+        };
+        defaultApplications = {
+          "text/*" = ["nvim.desktop"];
 
-        "image/*" = ["qimgv.desktop"]; # imv.desktop
+          "image/*" = ["qimgv.desktop" "imv.desktop"];
 
-        "video/*" = ["mpv.desktop"];
+          "video/*" = ["mpv.desktop"];
 
-        "inode/directory" = ["pcmanfm.desktop"];
+          "inode/directory" = ["org.gnome.Nautilus.desktop"];
 
-        "application/pdf" = ["helium.desktop"]; # zathura.desktop
-        "text/html" = ["helium.desktop"];
-        "x-scheme-handler/about" = ["helium.desktop"];
-        "x-scheme-handler/http" = ["helium.desktop"];
-        "x-scheme-handler/https" = ["helium.desktop"];
-        "x-scheme-handler/unknown" = ["helium.desktop"];
-        "x-scheme-handler/webcal" = ["helium.desktop"];
-        "x-scheme-handler/chrome" = ["helium.desktop"];
+          "application/pdf" = ["helium.desktop" "zathura.desktop"]; # zathura.desktop
+          "text/html" = ["helium.desktop"];
+          "x-scheme-handler/about" = ["helium.desktop"];
+          "x-scheme-handler/http" = ["helium.desktop"];
+          "x-scheme-handler/https" = ["helium.desktop"];
+          "x-scheme-handler/unknown" = ["helium.desktop"];
+          "x-scheme-handler/webcal" = ["helium.desktop"];
+          "x-scheme-handler/chrome" = ["helium.desktop"];
+        };
       };
     };
   };
